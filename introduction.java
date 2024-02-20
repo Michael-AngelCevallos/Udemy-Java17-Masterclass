2f7c7c22a2197220e7b17d8d506cc777c929c139
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2024-02-20 13:23:18 -0600

    Added to intro

--- introduction.java
+++ introduction.java
@@ -1,5 +1,16 @@
 
 // Download to windows
 
-// Jshell - run java commands directley in terminal without IDE
+/** Jshell - run java commands directley in terminal without IDE
+ * 
+ * 1. open cmd prompt on windows/ terminal on mac
+ * 2. type --->>  jshell
+ * 3. press enter
+ * 4. you can now run java output in t cmd prompt/terminal
+ * 
+ * example:  System.out.print("Hello World");
+ * prints in cmd prompt --> Hello World
+ *  */ 
+
+ 
 
