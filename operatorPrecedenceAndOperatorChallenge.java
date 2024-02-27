7b02c328e58cc34a761399b265804f8b05f9cce6
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2024-02-27 15:30:53 -0600

    Added

---
+++ operatorPrecedenceAndOperatorChallenge.java
@@ -0,0 +1,44 @@
+/* OPERATOR PRECEDENCE - 
+ * 
+ * 
+ * **Simple Assignment Operator - (=)
+ * 
+ * **Arithmetic Operators - 
+ * 
+ * (+) Additive operator (also used for String Concatenation)
+ * (-) Subtraction Operator 
+ * (*) Multiplication Operator
+ * (/) Division Operator
+ * (%) Remainder Operator 
+ * 
+ * 
+ * Unary Operators 
+ *  (+) Unary plus operator; indicates positive value (numbers are positive without this, however)
+ * 
+ *  (-) Unary minus operator, negates an expression
+ * 
+ *  (++) Increment operator; increments a value by 1
+ * 
+ *  (--) Decrement operator; decrements a value by 1 
+ * 
+ *  (!) Logical complement operator; inverts the value of a boolean
+ * 
+ * 
+ * ** Equality and Relational Operators
+ *  (==) - Equal-to value
+ *  (!=) - Not Equal To
+ *  (>)  - Greater Than
+ *  (>=) - Greater than or equal to
+ *  (<)  - Less Than 
+ *  (<=) - Less than or equal to
+ * 
+ * OPERATOR CHALLENGE -
+ */
+
+
+
+public class operatorPrecedenceAndOperatorChallenge {
+    public static void main(String[] args) {
+        
+    }
+}
