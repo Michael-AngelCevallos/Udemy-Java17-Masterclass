37d0bd0db218c5aa62b1d0da1f4c7cac9b360741
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2024-03-14 17:56:52 -0500

    half way

---
+++ whileLoopChallenge.java
@@ -0,0 +1,43 @@
+/* WHILE LOOP CHALLENGE - 
+
+STEP 1 - isEvenNumber - that takes a parameter of type int. its purpose is to determine if the arguent passed to the method is an even number or not.
+
+step 2 - return for the method if it's a even number, otherwise return false
+ * 
+ * 
+ */
+
+
+public class whileLoopChallenge {
+    public static void main(String[] args) {
+        System.out.println(isEvenNumber(6));
+
+        System.out.println(isEvenNumber(9));
+
+        int number = 4;
+        int finishNumber = 20;
+
+
+        while(number <= finishNumber){
+
+            // This will increment out count from 4 by 1
+            number++;
+
+            // This will then check to see if new number is odd number, if it is, will continue with code above an increment new nyumber by 1 then return result to terminal
+            if(!isEvenNumber(number)){
+                continue;
+            }
+                System.out.println("Even number " + number);
+            }
+        }
+    
+    
+
+
+public static boolean isEvenNumber(int yourNumber){
+    if(yourNumber % 2 == 0){
+        return true;
+    }
+    return false;
+}
+}
