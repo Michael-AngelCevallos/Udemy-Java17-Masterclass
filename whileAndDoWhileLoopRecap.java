c60ec926d76b3e631820d0f4c023b8a0563e73c8
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2024-03-14 21:29:52 -0500

    Finish Recap

---
+++ whileAndDoWhileLoopRecap.java
@@ -0,0 +1,26 @@
+/* While and Do While loop Recap -
+ * 
+ * While loop - condition is checked first, if true THEN code is ran
+ * 
+ *  while loop synatx:
+ * 
+ * int count = 1;  <--- Intialized outside of loop
+ *  while(count <= 5){
+ *  System.out.println("count = " + count);
+ * count++;   <--- Incremenet
+ * }
+ * 
+ * ==================================
+ * 
+ * Do While loop - code is ran first then conditio is checked if true
+ * 
+ *  do while loop syntax:
+ * 
+ * do {
+ *  -- statemnets
+ * } while (condition);
+ */
+
+public class whileAndDoWhileLoopRecap {
+    
+}
